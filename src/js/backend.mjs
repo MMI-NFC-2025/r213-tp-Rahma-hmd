b1f2e3c0795897f8e20947c159eec60b2e36d175
Rahma <241714427+Rahma-hmd@users.noreply.github.com>
2026-02-01 10:47:33 +0100

    Maison pocket base fonction ofrre

---
+++ src/js/backend.mjs
@@ -0,0 +1,44 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: '-created',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function oneID(id) {
+    const record = await pb.collection('Maison').getOne(id);
+    return record;
+}
+
+export async function allMaisonsFavori() {
+    const records = await pb.collection('Maison').getFullList({ filter: 'favoris = true' });
+    return records;
+}
+
+export async function allMaisonsSorted() {
+    const record = await pb.collection('Maison').getFullList({ sort: 'Prix' });
+    return record;
+}
+
+export async function bySurface(s) {
+    const record = await pb.collection('Maison').getFullList({ filter: `superficie > ${s}` });
+    return record;
+}
+
+export async function surfaceORprice(s, p) {
+    const record = await pb.collection('Maison').getFullList({ filter: `superficie  ${s} || Prix < ${p}` });
+    return record;
+}
+
+export async function oneAgent(id) {
+    const record = await pb.collection('Agent').getOne(id);
+    return record;
+}
