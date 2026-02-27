2fe08435468c5b129d9063d9bf539488efc2a7cc
Rahma <241714427+Rahma-hmd@users.noreply.github.com>
2026-02-27 11:42:11 +0100

    adaptation netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  adapter: vercel()
-});
+  adapter: netlify(),
+}); 
